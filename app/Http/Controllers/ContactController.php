343d68b5fec88d3318cd6ce176ecb6850e3a0cb5
kk0055 <rtsw.lgy@gmail.com>
2020-10-15 14:02:52 +0800

    Completed

--- app/Http/Controllers/ContactController.php
+++ app/Http/Controllers/ContactController.php
@@ -14,7 +14,8 @@ class ContactController extends Controller
      */
     public function index()
     {
-        //
+        $contacts = Contact::all();
+        return view('contacts.index',compact('contacts'));
     }
 
     /**
@@ -45,6 +46,8 @@ class ContactController extends Controller
             'last_name'=> $request->get('last_name'),
             'email'=> $request->get('email'),
             'company' => $request->get('company'),
+            'city' => $request->get('city'),
+            'country' => $request->get('country'),
         ]);
         $contact->save();
         return redirect('/contacts')->with('success','Contact saved!');
@@ -70,7 +73,8 @@ class ContactController extends Controller
      */
     public function edit($id)
     {
-        //
+        $contact = Contact::find($id);
+        return view('contacts.edit',compact('contact'));
     }
 
     /**
@@ -82,7 +86,22 @@ class ContactController extends Controller
      */
     public function update(Request $request, $id)
     {
-        //
+        $request->validate([
+            'first_name'=> 'required',
+            'last_name'=> 'required',
+            'email'=> 'required',
+        ]);
+        $contact =  Contact::find($id);
+        $contact->first_name = $request->get('first_name');
+        $contact->last_name = $request->get('last_name');
+        $contact->email = $request->get('email');
+        $contact->company = $request->get('company');
+        $contact->city = $request->get('city');
+        $contact->country = $request->get('country');
+        $contact->save();
+
+        return redirect('/contacts')->with('success', 'Contact updated');
+      
     }
 
     /**
@@ -93,6 +112,9 @@ class ContactController extends Controller
      */
     public function destroy($id)
     {
-        //
+        $contact = Contact::find($id);
+        $contact->delete();
+
+        return redirect('/contacts')->with('success', 'Contact deleted');
     }
 }
--- resources/views/contacts/edit.blade.php
+++ resources/views/contacts/edit.blade.php
@@ -16,31 +16,32 @@
       </div>
       <br>
        @endif
-     <form method="post" action="{{ route('contacts.store') }}">
-    @csrf
+     <form method="post" action="{{ route('contacts.update',$contact->id) }}">
+      @method('PATCH')
+      @csrf
   <div class="form-group">
     <label for="first_name">First Name:</label>
-  <input type="text" name="first_name" class="form-control" value="{{ old('first_name') }}">
+  <input type="text" name="first_name" class="form-control" value="{{ $contact->first_name}}">
   </div>    
   <div class="form-group">
     <label for="last_name">Last Name:</label>
-  <input type="text" name="last_name" class="form-control">
+  <input type="text" name="last_name" class="form-control" value="{{ $contact->last_name}}">
   </div>    
   <div class="form-group">
     <label for="email">Email:</label>
-  <input type="text" name="email" class="form-control">
+  <input type="text" name="email" class="form-control" value="{{ $contact->email}}">
   </div>    
   <div class="form-group">
     <label for="city">City:</label>
-  <input type="text" name="city" class="form-control">
+  <input type="text" name="city" class="form-control" value="{{ $contact->city}}">
   </div>    
   <div class="form-group">
     <label for="country">Country:</label>
-  <input type="text" name="country" class="form-control">
+  <input type="text" name="country" class="form-control" value="{{ $contact->country}}">
   </div>    
   <div class="form-group">
     <label for="company">Company:</label>
-  <input type="text" name="company" class="form-control">
+  <input type="text" name="company" class="form-control" value="{{ $contact->company}}">
   </div>    
   <button type="submit" class="btn btn-primary">Add Contact</button>
     </form>
